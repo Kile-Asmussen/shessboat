452235b3e58195c006e762605626c8611ab52896
Kile Asmussen <kile.asmussen@gmail.com>
2025-07-11 17:33:07 +0200

    .

--- src/board.rs
+++
@@ -1,9 +0,0 @@
-use crate::{
-    moves::GeneralMove,
-    sfen::SFen,
-    validity::{Validatable, Validator},
-};
-
-pub trait Board: Validator<GeneralMove> + Validatable {
-    fn new(fen: &SFen) -> Self;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-pub mod board;
-pub mod byteboard;
-pub mod castling;
-pub mod elements;
-pub mod moves;
-pub mod sfen;
-pub mod tempo;
-pub mod validity;
+mod bitboard;
 
-fn main() {}
+fn main() {
+    println!("Hello, world!");
+}

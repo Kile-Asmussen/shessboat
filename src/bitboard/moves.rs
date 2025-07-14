4f0166ddb8c05ccae35475bf79e35e7546e9b0fb
Kile Asmussen <kile.asmussen@gmail.com>
2025-07-14 17:53:56 +0200

    .

--- src/bitboard/moves.rs
+++ src/bitboard/moves.rs
@@ -1,4 +1,5 @@
 use crate::bitboard::{
+    CastlingRights,
     enums::{Color, Piece},
     squares::Square,
 };
@@ -8,7 +9,19 @@ pub struct Move {
     piece: Piece,
     from: Square,
     to: Square,
+    castling: Option<Castling>,
     capture: Option<(Square, Piece)>,
     en_passant: Option<Square>,
     promotion: Option<Piece>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Castling {
+    OOO = 1,
+    OO,
+}
+
+#[test]
+fn size_fuckery() {
+    println!("{}", std::mem::size_of::<Move>());
 }

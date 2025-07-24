37c3660d34838723c8dce5d7958062e50042e56f
Kile Asmussen <kile.asmussen@gmail.com>
2025-07-24 20:07:03 +0200

    .

--- src/engine.rs
+++ src/engine.rs
@@ -4,19 +4,3 @@ use crate::shessboard::{
     BitBoard,
     zobrist::{BitBoardHasher, PositionHashes},
 };
-
-pub struct ShessEngine<'a> {
-    pub hasher: &'a BitBoardHasher,
-    pub hashes: PositionHashes,
-    pub board: BitBoard,
-}
-
-impl<'a> ShessEngine<'a> {
-    pub fn new(hasher: &'a BitBoardHasher) -> Self {
-        Self {
-            hashes: HashMap::new(),
-            hasher,
-            board: BitBoard::empty(),
-        }
-    }
-}
